8dec805a5db4a6f486e4dd0646bff1eea89668fb
Eric Chhun <ericchhun@Erics-MacBook-Pro-6.local>
2026-06-17 18:57:55 -0700

    Add config py

--- config.py
+++ config.py
@@ -3,9 +3,17 @@
 
 
 class Config:
-    SECRET_KEY = 'your_secret_key_here'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///coffeebucks.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # Configuration settings for the flask application
+    SECRET_KEY = 'your_secret_key_here' # Secret key for session management and security
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///coffeebucks.db' # Database URI for SQLAlchemy, 
+    # using SQLite for simplicity
+    SQLALCHEMY_TRACK_MODIFICATIONS = False # Disable SQLAlchemy event system to save 
+    #resources and avoid warnings
+    
+class developmentConfig(Config):
+    # Development-specific configuration settings
+    DEBUG = True # Enable debug mode for development, provides detailed error pages 
+    # and auto-reloading of the server on code changes
 
 
 
