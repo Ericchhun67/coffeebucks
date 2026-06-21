fb4cbde1ea21bcbb9273176665b27ec32b7d609f
Eric Chhun <ericchhun@Erics-MacBook-Pro-6.local>
2026-06-21 15:58:01 -0700

    Add pages py

--- routes/pages.py
+++ routes/pages.py
@@ -9,6 +9,8 @@ Purpose: this file defines the routes for the main pages of the Good Days coffee
 
 from flask import Flask, Blueprint, render_template, request, redirect, url_for
 from extensions import db
+from models.feedback import Feedback
+from models.user import User
 
 # Blueprint for handling page routes
 pages_bp = Blueprint('pages', __name__)
@@ -22,6 +24,18 @@ def index() -> str:
 @pages_bp.route('/contact')
 def contact() -> str:
     return render_template('contact.html')
+
+# Route for the submit feedback form
+@pages_bp.route('/submit-feedback', methods=['POST'])
+def submit_feedback() -> str:
+    name = request.form.get('name')
+    email = request.form.get('email')
+    message = request.form.get('message')
+    
+    # get the feedback data and save it to the database
+    feedback = Feedback(name=name, email=email, message=message)
+    feedback.save_feedback()
+    return redirect(url_for('pages.contact'))
 
 
 # Route for the about page
